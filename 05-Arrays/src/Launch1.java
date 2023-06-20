661e6d7a984737b1c7b37ec7c775ca73d53a671d
Omprakash <omprakash8578502@gmail.com>
2023-06-20 10:02:49 +0530

    scanner object

---
+++ 05-Arrays/src/Launch1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Launch1 {
+
+	public static void main(String[] args) {
+		System.out.println("please enter Number");
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		
+		System.out.println("After taking input");
+		System.out.println(a);
+
+	}
+
+}
