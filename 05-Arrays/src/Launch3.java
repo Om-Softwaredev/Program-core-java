cfb19a364c704b14934ddb2303a6cd14fdceab88
Omprakash <omprakash8578502@gmail.com>
2023-06-20 13:13:14 +0530

    2D regular array

---
+++ 05-Arrays/src/Launch3.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Launch3 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("please enter no of classes : ");
+		int cls=scan.nextInt();
+		System.out.print("please enter no of student : ");
+		int std = scan.nextInt();
+		int[][] arr = new int[cls][std];//2D regular array
+		
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				System.out.print("Enter marks of class "+ i + " and student "+ j +" ");
+				arr[i][j]=scan.nextInt();
+			}
+		}
+		System.out.println("marks of the student");
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				System.out.print(arr[i][j]+" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
