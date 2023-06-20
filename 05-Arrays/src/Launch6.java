2c3798a5115cc29cf81ff71ccc92d0297b71d4ef
Omprakash <omprakash8578502@gmail.com>
2023-06-20 13:40:02 +0530

    creating Array for an Object

---
+++ 05-Arrays/src/Launch6.java
@@ -0,0 +1,21 @@
+//Creating Array for object
+
+class Fan{
+	int cost;
+	String brand;
+	int noOfWings;
+}
+public class Launch6 {
+
+	public static void main(String[] args) {
+		Fan[] f =new Fan[3];
+		f[0]=new Fan();
+		f[1]=new Fan();
+		f[2]=new Fan();
+		
+		f[0].brand="Bajaj";
+		f[0].cost=10000;
+
+	}
+
+}
