4de3da4a484168c826725b3c4259b9d16b18d71d
Omprakash <omprakash8578502@gmail.com>
2023-06-21 10:25:26 +0530

    Array Sum

---
+++ 05-Arrays/src/Launch8.java
@@ -0,0 +1,13 @@
+
+public class Launch8 {
+
+	public static void main(String[] args) {
+		int[] arr= {10,20,30,40};
+		int sum=0;
+		for (int i = 0; i < arr.length; i++) {
+			sum+=arr[i];
+		}
+		System.out.println(sum);
+	}
+
+}
